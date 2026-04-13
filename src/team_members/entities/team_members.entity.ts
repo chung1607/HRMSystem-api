deca649fdb0699a2c21b461e607557a9654fd09a
LaiVanChung <126126468+tothichcau02@users.noreply.github.com>
2026-04-13 22:19:01 +0700

    Update api crud teams #20

--- src/team_members/entities/team_members.entity.ts
+++ src/team_members/entities/team_members.entity.ts
@@ -1,27 +1,27 @@
-import { Teams } from 'src/teams/entities/teams.entity';
-import { User } from 'src/user/entities/user.entity';
-import { CreateDateColumn, Entity, JoinColumn, ManyToOne, OneToOne, PrimaryGeneratedColumn, Unique } from 'typeorm';
+// import { Teams } from 'src/teams/entities/teams.entity';
+// import { User } from 'src/user/entities/user.entity';
+// import { CreateDateColumn, Entity, JoinColumn, ManyToOne, OneToOne, PrimaryGeneratedColumn, Unique } from 'typeorm';
 
-@Entity('team_members')
-@Unique(['user'])
-export class Team_Members {
-  @PrimaryGeneratedColumn()
-  id: number;
+// @Entity('team_members')
+// @Unique(['user'])
+// export class Team_Members {
+//   @PrimaryGeneratedColumn()
+//   id: number;
 
-  @ManyToOne(() => Teams, (team) => team.members, {
-    onDelete: 'CASCADE',
-  })
-  @JoinColumn({ name: 'team_id' })
-  team: Teams;
+//   @ManyToOne(() => Teams, (team) => team.members, {
+//     onDelete: 'CASCADE',
+//   })
+//   @JoinColumn({ name: 'team_id' })
+//   team: Teams;
 
-  @OneToOne(() => User, (user) => user.teamMember, {
-    onDelete: 'CASCADE',
-  })
+//   @OneToOne(() => User, (user) => user.teamMember, {
+//     onDelete: 'CASCADE',
+//   })
 
-  @JoinColumn({ name: 'user_id' })
-  user: User;
+//   @JoinColumn({ name: 'user_id' })
+//   user: User;
 
-  @CreateDateColumn()
-  joined_at: Date;
+//   @CreateDateColumn()
+//   joined_at: Date;
 
-}
+// }
